cc0d2db32478c3b245c4317d17c89ccced46c697
MsHedgehod <byancacalixto1@gmail.com>
2019-06-26 17:25:39 -0300

    edicao1

---
+++ src/app/orcamento/orcamento.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrcamentoComponent } from '../orcamento/orcamento.component';
+
+@NgModule({
+  declarations: [OrcamentoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrcamentoModule { }
